fb35cd9e83f09fe1c857b714de9b975d8d3a1946
Stephenhua <41442321+Stephenhua@users.noreply.github.com>
2018-11-01 23:16:06 +0800

    ,,

---
+++ LeetCode_7/LeetCode_7/源.cpp
@@ -0,0 +1,67 @@
+# include <iostream>
+# include <math.h>
+using namespace  std;
+int reverse(int x)//方法二
+{
+	int ret = 0;
+	bool isNegative = false;
+	if (x < 0)
+	{
+		isNegative = true;
+		x *= -1;
+	}
+	while (x > 0)
+	{
+		int lastDigit = x % 10;
+		if (lastDigit == 0 && ret == 0)
+		{
+			x = x / 10;
+			continue;
+		}
+		if (ret > (INT_MAX - lastDigit) / 10)
+		{
+			return 0;
+		}
+		ret = (ret * 10 + lastDigit);
+		x = x / 10;
+
+	}
+	return ret*((isNegative) ? -1 : 1);
+}
+int reverse1(int x) {//进行数字反转可以实现方法一
+		int res = 0;
+		while (x != 0) {
+			if (abs(res) > INT_MAX / 10) return 0;
+			res = res * 10 + x % 10;
+			x /= 10;
+		}
+		return res;
+	}
+
+int reverse2(int x)
+{
+	bool a = (x > 0);
+	long x_long = (a) ? x : -x;
+	long ans = 0;
+	while (x_long > 0)
+	{
+		if (ans > INT_MAX / 10 || ans < INT_MIN / 10)
+		{
+			return 0;
+		}
+		ans = ans * 10 + x_long % 10;
+		x_long = x_long / 10;
+	}
+	return (a) ? ans : -ans;
+
+}
+
+int main()
+{
+	int x = 432492024;
+	cout << reverse(x) << endl;
+	cout << reverse1(x) << endl;
+	cout << reverse2(x) << endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
--- Longest_Substring/Longest_Substring/源.cpp
+++ Longest_Substring/Longest_Substring/源.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 # include <string.h>
 # include <algorithm>
+# include <unordered_map>
 using namespace std;
 
 	int lengthOfLongestSubstring(string s) {
@@ -18,6 +19,32 @@ using namespace std;
 	}
 	int main()
 	{
+		vector<int >list;
+		list.push_back(5);
+		list.push_back(14);
+		list.push_back(34);
+		list.push_back(22);
+		list.push_back(39);
+		list.push_back(5);
+		unordered_map<int, int> map;
+		for (int i = 0; i < list.size(); i++)
+		{
+			map[i] = list[i];
+		}
+		cout << map[0] << endl;
+		//for (std::unordered_map<int, int >::iterator i = map.begin; i != map.end(); i++)
+		//{
+		//	cout << i->first << ' ' << i->second << endl;
+
+		//}
+		if (map.find(3) != map.end())
+		{
+			cout << "find key= " << map.find(3)->first << " , value=" << map.find(3)->second << endl;
+		}
+		if (map.count(5) > 0)
+		{
+			cout << "find 5: " << map.count(5) << endl;
+		}
 		char a[] = "abacddfefsfdsfgsgefsfsgdfafafdf";
 		cout<<lengthOfLongestSubstring(a)<<endl;
 		system("pause");
